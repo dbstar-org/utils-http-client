34f1bef10bd935c056638478e65ff37835ad0239
dbstarll <dbstarll@gmail.com>
2023-03-20 13:48:19 +0800

    Async的proxy模式暂时无法实现

--- src/main/java/io/github/dbstarll/utils/http/client/HttpClientFactory.java
+++ src/main/java/io/github/dbstarll/utils/http/client/HttpClientFactory.java
@@ -179,9 +179,10 @@ public final class HttpClientFactory {
         if (sslContext != null) {
             builder.setTlsStrategy(ClientTlsStrategyBuilder.create().setSslContext(sslContext).build());
         }
-        if (proxy != null && proxy.type() == Type.SOCKS && resolveFromProxy) {
-            builder.setDnsResolver(new FakeDnsResolver());
-        }
+        // Async的proxy模式暂时无法实现
+//        if (proxy != null && proxy.type() == Type.SOCKS && resolveFromProxy) {
+//            builder.setDnsResolver(new FakeDnsResolver());
+//        }
         return builder.build();
     }
 
